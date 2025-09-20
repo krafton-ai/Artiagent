53ef96cf16ac039e994e44e503107ec6c7480dc0
Jaehyun Park <jhpark00813@gmail.com>
2025-09-20 20:48:03 +0900

    add/delete misc. scripts/files

--- src/animal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Make only GPU 0 and GPU 2 visible
-export CUDA_VISIBLE_DEVICES=1
-
-# Your command here
-./run_vlpart.sh animal --max-images 50 --output-dir ../exps/vlpart_animal_new
-./run_flux.sh  ../exps/vlpart_animal_new --output-dir ../exps/vlpart_animal_distort_flux
---
+++ src/scripts/data_gen_objects.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Make only GPU 0 and GPU 2 visible
+
+source ~/.bashrc
+
+# Initialize conda for shell use
+eval "$(conda shell.bash hook)"
+
+parent_dir="/data3/jhpark/image-artifact-real-images"
+data_dir="coco/objects"
+output_dir="/data3/jhpark/data_synth"
+filtered_output_dir="/data3/jhpark/filtered_data_synth"
+
+
+export CUDA_VISIBLE_DEVICES=1
+# Your command here
+# conda activate gsam
+# ./run_gsam.sh custom --dataset custom --dataset-path $parent_dir/$data_dir --output-dir $output_dir/$data_dir
+conda activate rf-solver
+./run_flux.sh  $output_dir/$data_dir --output-dir $output_dir/$data_dir
+conda activate gsam
+export CUDA_VISIBLE_DEVICES=1
+python unified_data_pipeline.py --gsam_dir $output_dir/$data_dir --flux_dir $output_dir/$data_dir --output_dir $filtered_output_dir/$data_dir
+# cd /data3/jhpark
